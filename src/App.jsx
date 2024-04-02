181f23e42094a142cd213adc85917dc72c027d79
TaneeshkaReddy <taneeshkareddy22@gmail.com>
2024-04-02 21:41:51 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Dogs from"./Dogs";
+
+function App() {
+  return <div>
+    <Dogs></Dogs>
+  </div>
+}
+
+export default App
---
+++ src/Dogs.jsx
@@ -0,0 +1,6 @@
+import "./Dogs.css";
+
+function Dogs(){
+  return <p class="dog">Bark bark!</p>;
+}
+export default Dogs
