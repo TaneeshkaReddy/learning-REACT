4c1572006dc60bf448132696a446e3bdb07cdbe5
TaneeshkaReddy <taneeshkareddy22@gmail.com>
2024-04-04 00:45:35 +0530

    continuing dynamic styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Greeter from "./Greeter";
 import Die from "./Die";
 import ListPicker from "./ListPicker";
 import DoubleDice from "./DoubleDice";
+import Heading from "./Heading";
 
 function App() {
   return <div>
+    <Heading color="magenta" text="Practise Baby"></Heading>
+    <Heading color="magenta" text="Practise Makes A Bitch Perfect"></Heading>
+    <hr></hr>
     <h1>GREETER COMPONENT</h1>
     <Greeter person="TanTanaTan" from="Ed Sheeran"></Greeter>
     <hr></hr>
@@ -21,7 +25,7 @@ function App() {
     <ListPicker values={["a","b","c"]}></ListPicker>
     {/* <ListPicker values={{a:2,b:3}}></ListPicker>  JS OBJECT CAN BE PASSED LIKE THIS  */}
 
-
+   <hr></hr>
    <h1>DOUBLEDICE COMPONENT</h1>
    <DoubleDice></DoubleDice>
 
